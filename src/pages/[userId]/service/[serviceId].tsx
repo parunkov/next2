5dada8e1e579a492940d80397d44db7b3d15c3a1
parunkov <parunkov@gmail.com>
2025-02-16 12:47:33 +0500

    icons file

--- src/pages/[userId]/service/[serviceId].tsx
+++ src/pages/[userId]/service/[serviceId].tsx
@@ -3,6 +3,7 @@ import styles from "./Service.module.scss";
 import Aside from "@/components/Aside";
 import MobileHeader from "@/components/MobileHeader/MobileHeader";
 import Breadcrumbs from "@/components/Breadcrumbs/Breadcrumbs";
+import { editImg, deleteImg, falmeImg, safeImg } from "@/icons/serviceIcons";
 
 const ServicePage: FC = () => {
   const breadcrumbItems = [
@@ -22,7 +23,7 @@ const ServicePage: FC = () => {
             prof="Fullstack Web-developer"
             name="Darrell Steward"
             username="vladweb"
-            serviceTime="1 год"
+            serviceTime="На сервисе 1 год"
             totalOrders={345}
             reviews={{
               positive: 34,
@@ -30,7 +31,34 @@ const ServicePage: FC = () => {
             }}
             activeOrders={4}
           />
-          <div className={styles.portfolioContent}></div>
+          <div className={styles.serviceContent}>
+            <div className={styles.serviceHeader}>
+              <h1>Разработка веб сайта</h1>
+              <div className={styles.actions}>
+                <button className={styles.edit} dangerouslySetInnerHTML={{ __html: editImg }} />
+                <button className={styles.delete} dangerouslySetInnerHTML={{ __html: deleteImg }} />
+              </div>
+            </div>
+
+            <div className={styles.serviceTags}>
+              <span className={styles.tag}>
+                <span dangerouslySetInnerHTML={{ __html: falmeImg }} />
+                Срочный заказ
+              </span>
+              <span className={styles.tag}>
+                <span dangerouslySetInnerHTML={{ __html: safeImg }} />
+                Безопасная сделка
+              </span>
+            </div>
+
+            <div className={styles.price}>
+              100 000 ₽/проект
+            </div>
+
+            <div className={styles.serviceDescription}>
+              Figma connection vector asset line. Thumbnail outline asset line. Thumbnail outline asset line. Prototype flatten line. Align hand connection vector effect. Selection connection clip polygon blur. Stroke variant slice team font. Lorem ipsum effect. Stroke effect align. Ellipse draft pixel.Underline asset thumbnail line share. Thumbnail outline create fill figma hand. Align align image follower vector project. Rotate flatten fill connection union device. Connection boolean group follower outline line. Lorem line hand create fill. Layer frame hand create select.
+            </div>
+          </div>
         </div>
       </div>
     </div>
